85b0096b417d373dc0acb583268f9b24dfd4f80a
Nicole T <nbtrieu@uci.edu>
2022-12-11 22:30:51 -0800

    Create server.js and GET routes for homepage, notes, and 404

---
+++ routes/notes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const notes = express.Router();
+
+notes.get('/', (req, res) => {
+    
+})
---
+++ server.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const express = require('express');
+// const api = require('./routes/index.js')
+const path = require('path');
+
+const PORT = 3001;
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET route for homepage
+app.get('/', (req, res) => {
+  console.log('Routing to homepage');
+  res.sendFile(path.join(dir__name, 'public', 'index.html'));
+})
+
+// GET route for note taking page
+app.get('/notes', (req, res) => {
+  console.log('Routing to note taking page');
+  res.sendFile(path.join(dir__name, 'public', 'notes.html'));
+})
+
+// GET route for 404 error page
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', '404.html'));
+})
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT} 🚀`);
+})
