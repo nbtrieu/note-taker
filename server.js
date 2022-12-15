a1eb8630ad555cb77e287963e8872c60608259cf
Nicole T <nbtrieu@uci.edu>
2022-12-14 23:51:09 -0800

    Pushing to Heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const apiRoutes = require('./routes/index.js')
 const path = require('path');
 
-const PORT = 3001;
+const port = process.env.PORT || 3001;
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
